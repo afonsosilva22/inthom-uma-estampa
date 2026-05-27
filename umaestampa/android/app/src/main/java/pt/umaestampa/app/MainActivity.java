21809c4e0f452cb7645629823c41b9299c2cec17
afonsosilva22 <limasilva.afonso@gmail.com>
2026-05-27 19:36:11 +0100

    update for physical deployment

---
+++ umaestampa/android/app/src/main/java/pt/umaestampa/app/MainActivity.java
@@ -0,0 +1,5 @@
+package pt.umaestampa.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- umaestampa/capacitor.config.ts
+++ umaestampa/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'umaestampa',
+  appId: 'pt.umaestampa.app',
+  appName: 'Uma Estampa',
   webDir: 'www'
 };
 
